cf1d908f6fbef468341bde561b9ae31dd5c60521
lasantha <lasantha.p@tailwindsw.com>
2023-05-19 22:06:47 +0530

    first commit

---
+++ src/autoboxingandunboxing/AutoboxingAndUnboxing.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package autoboxingandunboxing;
+
+import java.util.ResourceBundle;
+
+/**
+ *
+ * @author user
+ */
+public class AutoboxingAndUnboxing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        
+             String a = "A";
+             String b = "B";
+             System.out.println(ResourceBundle.getBundle("UK"));
+            
+    }
+    
+}
